b2aa81c0e2836bd9436192e9336d60b3bcdbc66b
bhanuprakaash <bhanuprakashsai7@gmail.com>
2025-12-29 11:59:16 +0530

    3.1: comma

--- ch3/strings/main.go
+++ ch3/strings/main.go
@@ -1,41 +1,51 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"unsafe"
+	"strings"
 )
 
 func main() {
-	var x string = "Hello, world"
-	var y string = "Hello, world"
-	// var str string = x[2:5]
-
-	fmt.Println(x[2])
-
-	fmt.Println(x[2:4])
-
-	fmt.Println(x[:])
-
-	fmt.Println(string(x[0]) < string(y[0]))
-
-	fmt.Println(x + y + "3")
-
-	fmt.Println("H" > "h")
-
-	fmt.Printf("%p\n", unsafe.StringData(x))
-	fmt.Printf("%p\n", unsafe.StringData(y))
-
-	var lit string
-
-	lit = `SELECT * FROM employees WHERE id=2;`
-
-	fmt.Println(lit)
-
-	for _, b := range []byte(string('❌')) {
-		fmt.Printf("%08b ", b)
+	inputs := []string{
+		"1234567",     // Standard integer
+		"-12345.67",   // Negative float
+		"+123456.789", // Positive sign float
+		"12",          // Short integer
+		"123.456",     // Short integer with float
+		"-1234",       // Negative integer
 	}
 
-	s := []byte(x)
-	fmt.Println(s)
+	for _, s := range inputs {
+		fmt.Printf("Input: %-12s | Output: %s\n", s, comma(s))
+	}
+}
 
+func comma(s string) string {
+	var buf bytes.Buffer
+
+	start := 0
+	if len(s) > 0 && (s[0] == '+' || s[0] == '-') {
+		buf.WriteByte(s[0])
+		start = 1
+	}
+	end := strings.Index(s, ".")
+	if end == -1 {
+		end = len(s)
+	}
+	integerPart := s[start:end]
+	n := len(integerPart)
+
+	for i, c := range integerPart {
+		if i > 0 && (n-i)%3 == 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteRune(c)
+	}
+
+	if end < len(s) {
+		buf.WriteString(s[end:])
+	}
+
+	return buf.String()
 }
