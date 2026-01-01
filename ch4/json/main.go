9b1ab3d434ceb3eba258b21e02614c421aaa0d59
bhanuprakaash <bhanuprakashsai7@gmail.com>
2026-01-01 21:25:50 +0530

    json

---
+++ ch4/json/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+}
+
+func main() {
+
+	data, err := json.Marshal(movies)
+	if err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Println(string(data))
+
+	var result []Movie
+	if err := json.Unmarshal(data, &result); err != nil {
+		log.Fatalf("JSON marshaling failed: %s", err)
+	}
+	fmt.Println(result)
+
+	for _, e := range result {
+		fmt.Printf("%s\n", e.Title)
+	}
+
+}
