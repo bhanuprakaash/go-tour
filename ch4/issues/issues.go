ca4558cada6c941bba498fe6975656e45d370aed
bhanuprakaash <bhanuprakashsai7@gmail.com>
2026-01-02 22:23:43 +0530

    html and text templates

--- ch4/issues/issues.go
+++ ch4/issues/issues.go
@@ -1,22 +1,56 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"os"
-	"time"
+	// "time"
 
 	"github.com/bhanuprakaash/go-tour.git/ch4/github"
 )
+
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}----------------------------------------
+Number: {{.Number}}
+User: {{.User.Login}}
+Title: {{.Title | printf "%.64s"}}
+Age: {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+// func daysAgo(t time.Time) int {
+// 	return int(time.Since(t).Hours() / 24)
+// }
+
+// var report = template.Must(template.New("issuelist").
+// 	Funcs(template.FuncMap{"daysAgo": daysAgo}).
+// 	Parse(templ))
+
+var issueList = template.Must(template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues</h1>
+<table>
+<tr style='text-align: left'>
+<th>#</th>
+<th>State</th>
+<th>User</th>
+<th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+<td><a href='{{.HTMLURL}}'>{{.Number}}</td>
+<td>{{.State}}</td>
+<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
 
 func main() {
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s %v\n",
-			item.Number, item.User.Login, item.Title, item.CreatedAt.Format(time.DateOnly))
+	if err := issueList.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
 	}
 }
