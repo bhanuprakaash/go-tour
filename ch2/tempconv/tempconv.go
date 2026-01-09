51d53364b63bbca2eb01c6effb65216c9430a128
bhanuprakaash <bhanuprakashsai7@gmail.com>
2026-01-09 22:49:42 +0530

    interface satisfaction

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -1,10 +1,14 @@
 package tempconv
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 type Celsius float64
 type Fahrenheit float64
 type Kelvin float64
+type celsiusFlag struct{ Celsius }
 
 const (
 	AbsoluteZeroC Celsius = -273.15
@@ -15,3 +19,24 @@ const (
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 func (k Kelvin) String() string     { return fmt.Sprintf("%g°K", k) }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	}
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
